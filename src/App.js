289c38b16343946a56a4b6cff8b2b5bba528bac8
fidellim <lim.fidel@gmail.com>
2022-10-19 22:43:50 +0400

    add bg color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg'
 
 function App() {
     return (
-        <div className="flex justify-center items-center bg-slate-800 min-w-screen min-h-screen">
+        <div className="flex justify-center items-center bg-darkPurple bg-pattern-hills bg-bottomAndCenter bg-no-repeat  bg-[length:contain,contain] min-w-screen min-h-screen">
             <header className="flex flex-col justify-center items-center text-white">
                 <img
                     src={logo}
