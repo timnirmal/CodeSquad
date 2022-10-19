8567bfac458baefaa14758f93a36638229cf3f20
fidellim <lim.fidel@gmail.com>
2022-10-19 17:43:37 +0400

    configure tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                grayishBlue: 'hsl(var(--grayishBlue) / <alpha-value>)',
+                softRed: 'hsl(var(--softRed) / <alpha-value>)',
+                default: 'hsl(var(--default) / <alpha-value>)',
+                darkDesaturatedBlue:
+                    'hsl(var(--darkDesaturatedBlue) / <alpha-value>)',
+                veryDarkBlue: 'hsl(var(--veryDarkBlue) / <alpha-value>)',
+                veryDarkBlackBlue:
+                    'hsl(var(--veryDarkBlackBlue) / <alpha-value>)',
+            },
+            fontFamily: {
+                redHat: ['var(--redHat)'],
+            },
+        },
     },
     plugins: [],
 }
