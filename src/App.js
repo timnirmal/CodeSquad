f2008afcf89687de1c314333a20c29ace3b77d67
fidellim <lim.fidel@gmail.com>
2022-10-22 16:51:27 +0400

    style page and add card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import logo from './logo.svg'
+import {
+    top_card_bottom_left_circle,
+    top_card_bottom_right_circle,
+    bottom_card_bottom_left_circle,
+    bottom_card_bottom_right_circle,
+    card_overlay,
+} from './constants'
 
 function App() {
     return (
-        <div className="flex justify-center items-center bg-darkPurple bg-pattern-hills bg-bottomAndCenter bg-no-repeat  bg-[length:contain,contain] min-w-screen min-h-screen">
-            <header className="flex flex-col justify-center items-center text-white">
-                <img
-                    src={logo}
-                    className="w-[150px] h-[150px] animate-[spin_5s_linear_infinite]"
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <main className="flex flex-col justify-center items-center bg-darkPurple bg-pattern-hills bg-bottomAndCenter bg-no-repeat  bg-[length:contain,contain] min-w-screen min-h-screen">
+            <h1 className="text-default uppercase tracking-[.35rem]">
+                We're launching soon
+            </h1>
+            <div className="flex ">
+                <div className="">
+                    <div
+                        className={`relative inline-flex flex-col text-softRed text-[4rem] ${card_overlay}`}
+                    >
+                        <h2
+                            className={`relative h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle}`}
+                        >
+                            08
+                        </h2>
+                        <h2
+                            className={`relative flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}`}
+                        >
+                            08
+                        </h2>
+                    </div>
+                    <h3>Days</h3>
+                </div>
+            </div>
+        </main>
     )
 }
 
