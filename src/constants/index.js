35b07392c73863db361a737957de5969792ed61c
fidellim <lim.fidel@gmail.com>
2022-10-22 16:46:43 +0400

    add styles for card overlay and small circles in card

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+import {
+    top_card_bottom_left_circle,
+    top_card_bottom_right_circle,
+    bottom_card_bottom_left_circle,
+    bottom_card_bottom_right_circle,
+    card_overlay,
+} from './styles'
+
+export {
+    top_card_bottom_left_circle,
+    top_card_bottom_right_circle,
+    bottom_card_bottom_left_circle,
+    bottom_card_bottom_right_circle,
+    card_overlay,
+}
