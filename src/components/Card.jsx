3d536ab5defc15e63d15e23ab619971e5cb7f754
fidellim <lim.fidel@gmail.com>
2022-10-23 02:52:58 +0400

    add animation to each card and update width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import {
     top_card_bottom_left_circle,
     top_card_bottom_right_circle,
@@ -6,32 +7,33 @@ import {
     card_overlay,
 } from '../constants'
 
-const Card = ({ time, text }) => {
+const Card = forwardRef(({ time, prevTime, text }, ref) => {
     return (
-        <div className="flex-1 flex flex-col items-center gap-[1.5rem]">
+        <div className="flex flex-col items-center gap-[1.5rem]">
             <div
-                className={`flip-card relative inline-flex flex-col text-softRed text-[4rem] ${card_overlay}`}
+                className={`flip-card relative inline-flex flex-col text-softRed text-[5rem] ${card_overlay}`}
+                ref={ref}
             >
                 <div className="container-top relative">
                     <div
-                        className={`relative h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} `}
+                        className={`relative h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md text-center min-w-[150px] ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} top`}
                     >
                         {time.toString().padStart(2, '0')}
                     </div>
                     <div
-                        className={`absolute top-0 left-0 h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} top-flip`}
+                        className={`absolute top-0 left-0 h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md text-center min-w-[150px] ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} top-flip`}
                     >
-                        {time.toString().padStart(2, '0')}
+                        {prevTime.toString().padStart(2, '0')}
                     </div>
                 </div>
                 <div className="container-bottom relative">
                     <div
-                        className={`relative flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}  `}
+                        className={`relative flex justify-center items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md text-center min-w-[150px] ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle} bottom `}
                     >
-                        {time.toString().padStart(2, '0')}
+                        {prevTime.toString().padStart(2, '0')}
                     </div>
                     <div
-                        className={`absolute top-0 left-0 flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}  bottom-flip`}
+                        className={`absolute top-0 inset-x-0 flex justify-center items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md text-center min-w-[150px] ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}  bottom-flip`}
                     >
                         {time.toString().padStart(2, '0')}
                     </div>
@@ -42,6 +44,6 @@ const Card = ({ time, text }) => {
             </h3>
         </div>
     )
-}
+})
 
 export default Card
