72ad45d1855c4f6149c1db79597aad0229cb2df8
fidellim <lim.fidel@gmail.com>
2022-10-22 16:50:41 +0400

    update size

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,13 +1,13 @@
 export const top_card_bottom_left_circle =
-    'before:block before:absolute before:w-[3px] before:h-[3px] before:rounded-full before:bottom-0 before:left-0 before:-translate-x-[50%] before:translate-y-[50%] before:z-10 before:bg-veryDarkBlue'
+    'before:block before:absolute before:w-[12.5px] before:h-[12.5px] before:rounded-full before:bottom-0 before:left-0 before:-translate-x-[50%] before:translate-y-[50%] before:z-10 before:bg-veryDarkBlue'
 export const top_card_bottom_right_circle =
-    'after:block after:absolute after:w-[3px] after:h-[3px] after:rounded-full after:bottom-0 after:right-0 after:translate-x-[50%] after:translate-y-[50%] after:z-10 after:bg-veryDarkBlue'
+    'after:block after:absolute after:w-[12.5px] after:h-[12.5px] after:rounded-full after:bottom-0 after:right-0 after:translate-x-[50%] after:translate-y-[50%] after:z-10 after:bg-veryDarkBlue'
 
 export const bottom_card_bottom_left_circle =
-    'before:block before:absolute before:w-[3px] before:h-[3px] before:rounded-full before:top-0 before:left-0 before:-translate-x-[50%] before:-translate-y-[50%] before:z-10 before:bg-veryDarkBlue'
+    'before:block before:absolute before:w-[12.5px] before:h-[12.5px] before:rounded-full before:top-0 before:left-0 before:-translate-x-[50%] before:-translate-y-[50%] before:z-10 before:bg-veryDarkBlue'
 
 export const bottom_card_bottom_right_circle =
-    'after:block after:absolute after:w-[3px] after:h-[3px] after:rounded-full after:top-0 after:right-0 after:translate-x-[50%] after:-translate-y-[50%] after:z-10 after:bg-veryDarkBlue'
+    'after:block after:absolute after:w-[12.5px] after:h-[12.5px] after:rounded-full after:top-0 after:right-0 after:translate-x-[50%] after:-translate-y-[50%] after:z-10 after:bg-veryDarkBlue'
 
 export const card_overlay =
-    'before:block before:absolute before:rounded-sm before:inset-x-0 before:h-[10px] before:-bottom-[0px] before:translate-y-[.125rem] before:bg-veryDarkBlackBlue'
+    'before:block before:absolute before:rounded-md before:inset-x-0 before:h-[30px] before:-bottom-[5px] before:translate-y-[.125rem] before:bg-veryDarkBlackBlue'
