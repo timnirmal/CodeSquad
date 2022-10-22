0871947f301f1af887cf473764cde8b9940750bb
fidellim <lim.fidel@gmail.com>
2022-10-22 18:06:49 +0400

    add animation

--- src/App.js
+++ src/App.js
@@ -15,18 +15,32 @@ function App() {
             <div className="flex ">
                 <div className="">
                     <div
-                        className={`relative inline-flex flex-col text-softRed text-[4rem] ${card_overlay}`}
+                        className={`flip-card relative inline-flex flex-col text-softRed text-[4rem] ${card_overlay}`}
                     >
-                        <h2
-                            className={`relative h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle}`}
-                        >
-                            08
-                        </h2>
-                        <h2
-                            className={`relative flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}`}
-                        >
-                            08
-                        </h2>
+                        <div className="container-top relative">
+                            <div
+                                className={`relative h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} `}
+                            >
+                                08
+                            </div>
+                            <div
+                                className={`absolute top-0 left-0 h-[.75em] px-[.25em] pb-[0em] pt-[.25em] bg-darkDesaturatedBlue brightness-[83%] overflow-hidden leading-none rounded-t-md ${top_card_bottom_left_circle} ${top_card_bottom_right_circle} top-flip`}
+                            >
+                                08
+                            </div>
+                        </div>
+                        <div className="container-bottom relative">
+                            <div
+                                className={`relative flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}  `}
+                            >
+                                08
+                            </div>
+                            <div
+                                className={`absolute top-0 left-0 flex items-end h-[.75em] px-[.25em] pt-[0em] pb-[.25em] bg-darkDesaturatedBlue overflow-hidden leading-none rounded-b-md ${bottom_card_bottom_left_circle} ${bottom_card_bottom_right_circle}  bottom-flip`}
+                            >
+                                08
+                            </div>
+                        </div>
                     </div>
                     <h3>Days</h3>
                 </div>
