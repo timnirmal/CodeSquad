2a1198d2767c8d6ec0b9b9925d2433645e5c34b1
fidellim <lim.fidel@gmail.com>
2022-10-23 14:42:02 +0400

    import and export logo assets

---
+++ src/constants/assets.js
@@ -0,0 +1,5 @@
+import facebook from '../assets/icon-facebook.svg'
+import instagram from '../assets/icon-instagram.svg'
+import pinterest from '../assets/icon-pinterest.svg'
+
+export { facebook, instagram, pinterest }
